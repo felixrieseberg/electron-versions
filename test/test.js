4bb0e6d232c1e41822490a497744e22f7581336a
Felix Rieseberg <felix@felixrieseberg.com>
2021-03-01 18:03:05 -0800

    test: Fix the test

--- test/test.js
+++ test/test.js
@@ -1,13 +1,28 @@
-const { execSync } = require('child_process');
+const { execSync, exec } = require('child_process');
 const path = require('path');
+const fs = require('fs');
 
-const expected = `Tag v2.0.0  Electron 10.1.0  Chromium 85.0.4183.87
+const expected = `Tag master  Electron 10.1.0  Chromium 85.0.4183.87
+Tag v2.0.0  Electron 10.1.0  Chromium 85.0.4183.87
 Tag v1.1.0  Electron 9.0.5   Chromium 83.0.4103.119
 Tag v1.0.0  Electron 9.0.0   Chromium 83.0.4103.64`.trim();
 
+const script = path.join(__dirname, '../src/bin.js');
+const harness = path.join(__dirname, 'harness');
+
+function prep() {
+  const and = process.platform === 'win32' ? ';' : '&&'
+
+  // Unzip
+  if (!fs.existsSync(harness)) {
+    execSync(`cd ${__dirname} ${and} unzip ${harness}.zip`)
+  }
+
+  // Checkout master
+  execSync(`cd ${harness} ${and} git checkout master`);
+}
+
 function test() {
-  const script = path.join(__dirname, '../src/bin.js');
-  const harness = path.join(__dirname, 'harness');
   const result = execSync(`node ${script} ${harness}`).toString().trim();
 
   if (result === expected) {
@@ -23,4 +38,5 @@ function test() {
   }
 }
 
+prep();
 test();
